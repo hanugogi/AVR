0754b15dfe22a9b7652cf6ae5ab43b65b7c0ae75
unknown <hyinwoojjang@gmail.com>
2018-10-17 21:36:49 +0900

    Add LCD source

---
+++ 4. Pedestrian-based Traffic system/LCD.c
@@ -0,0 +1,84 @@
+﻿/*
+ * LCD.c
+ *
+ * Created: 2018-10-15 오후 4:10:11
+ *  Author: KHW
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void COMMAND(unsigned char byte);
+void DATA(unsigned char byte);
+void DATA_Str(char *str);
+void LCD_Init(void);
+
+void COMMAND(unsigned char byte){
+	_delay_ms(2);
+	
+	PORTD = byte & 0xF0;
+	PORTD &= 0xFC;
+	_delay_us(1);
+	
+	PORTD |= 0x04;
+	_delay_us(1);
+	
+	PORTD &= 0xFB;
+	PORTD = (byte << 4) & 0xF0;
+	PORTD &= 0xFC;
+	_delay_us(1);
+	
+	PORTD |= 0x04;
+	_delay_us(1);
+	
+	PORTD &= 0xFB;
+	
+}
+
+void DATA(unsigned char byte){
+	_delay_ms(2);
+	
+	PORTD = byte & 0xF0;
+	PORTD |= 0x01;
+	
+	PORTD &= 0xFD;
+	_delay_us(1);
+	
+	PORTD |= 0x04;
+	_delay_us(1);
+	
+	PORTD &= 0xFC;
+	PORTD = (byte << 4) & 0xF0;
+	PORTD |= 0x01;
+	PORTD &= 0xFD;
+	_delay_us(1);
+	
+	PORTD |= 0x04;
+	_delay_us(1);
+	
+	PORTD &= 0xFB;
+}
+
+void LCD_Init(){
+	_delay_ms(30);
+	
+	COMMAND(0x28);
+	_delay_us(39);
+	
+	COMMAND(0x0C);
+	_delay_us(39);
+	
+	COMMAND(0x01);
+	_delay_ms(1.53);
+	
+	COMMAND(0x06);
+}
+
+void DATA_Str(char *str){
+	while(*str){
+		DATA(*str);
+		str++;
+	}
+}
+
+void DATA_Num(int )
