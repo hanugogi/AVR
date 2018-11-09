2ed74f0a9f1236a688835d2221ad6d25b60cdfc3
unknown <hyinwoojjang@gmail.com>
2018-11-09 23:28:35 +0900

    Joystick Working Now (Insert delay bitween ADMUX changing/Remove extern keyword in return valueable)

--- 4. Pedestrian-based Traffic system/Joystick.c
+++ 4. Pedestrian-based Traffic system/Joystick.c
@@ -10,12 +10,12 @@
 #include <avr/interrupt.h>
 
 char XorY = 0;
-int coordinate[2] = {-10, -10};
+int coordinate[2];
 
 ISR(ADC_vect){
 	switch (XorY){
-		case 112: coordinate[0] = (ADCH << 8) + ADCL; break;
-		case 113: coordinate[1] = (ADCH << 8) + ADCL; break;
+		case 112: coordinate[0] = ADC; break;
+		case 113: coordinate[1] = ADC; break;
 		default: coordinate[0] = -1; coordinate[1] = -1; break;
 	}
 }
@@ -24,17 +24,17 @@ void Joystick_Init(void){
 	DDRF = 0x00;
 	
 	ADMUX = 0b10000000;		//AVCC, ADC0
-	ADCSRA = 0b10001101;	//ADC INT Enable, 64 Div
+	ADCSRA = 0b10001111;	//ADC INT Enable, 64 Div
 	
 	sei();
-	
-	ADCSRA |= 0x04;
 }
 
 int* Joystick_Read(void){
 	XorY = 112;
 	ADMUX &= 0b11100000;	//Reset ADCMUX
 	ADCSRA |= 0x40;			//Start ADC0 Conversion
+	
+	_delay_ms(40);
 	
 	XorY = 113;
 	ADMUX &= 0b11100000;	//Reset ADCMUX
